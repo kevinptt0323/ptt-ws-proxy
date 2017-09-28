e2acebe2ff5c2d9e71a6045b61117f42823c9a69
Kevin Lin <kevinptt0323@gmail.com>
2017-09-29 00:40:44 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  server: {
+    host: '127.0.0.1',
+    port: 9770,
+  },
+  silent: false,
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const config = require('./config');
+
+const logging = config.silent ? () => {} : console.log;
+
+const proxy = httpProxy.createProxyServer({
+  target: 'wss://ws.ptt.cc/bbs',
+  changeOrigin: true,
+  headers: {
+    origin: 'https://www.ptt.cc',
+  }
+});
+
+const proxyServer = http.createServer((req, res) => {
+  proxy.web(req, res);
+}).listen(config.server);
+
+proxyServer
+  .on('upgrade', (req, socket, head) => {
+    const remoteAddress =
+      req.headers['x-forwarded-for'] || 
+      socket.remoteAddress;
+    logging(`connection from ${remoteAddress}`);
+    proxy.ws(req, socket, head);
+  })
+  .on('listening', () => {
+    logging(`listening on ${config.server.host}:${config.server.port}`);
+  })
+;
