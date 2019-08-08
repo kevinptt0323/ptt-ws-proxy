cce6ddf26e8488a0c29c8a982e209e9981c408e3
Kevin Lin <kevinptt0323@gmail.com>
2019-08-08 17:15:03 +0800

    handle websocket connection by http-proxy with "ws" option

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const http = require('http');
 const httpProxy = require('http-proxy');
 
 const config = require('./config');
@@ -8,28 +7,25 @@ const logging = config.silent ? () => {} : console.log;
 
 const proxy = httpProxy.createProxyServer({
   target: 'wss://ws.ptt.cc/bbs',
+  ws: true,
   changeOrigin: true,
   headers: {
     origin: 'https://www.ptt.cc',
-  }
-});
-
-const proxyServer = http.createServer((req, res) => {
-  proxy.web(req, res);
+  },
 }).listen(config.server);
 
-proxyServer
-  .on('upgrade', (req, socket, head) => {
-    const remoteAddress =
-      req.headers['x-forwarded-for'] || 
-      socket.remoteAddress;
-    logging(`connection from ${remoteAddress}`);
-    proxy.ws(req, socket, head);
+proxy
+  .on('proxyReqWs', (proxyRes, req, socket) => {
+    const addr = req.headers['x-forwarded-for'] || socket.remoteAddress;
+    logging(`connection from ${addr}`);
   })
-  .on('listening', () => {
-    logging(`listening on ${config.server.host}:${config.server.port}`);
+  .on('open', (proxySocket) => {
+    logging(`connected to ${proxySocket.remoteAddress}`);
+  })
+  .on('close', (res, socket, head) => {
+    logging('connection closed');
   })
   .on('error', (err) => {
     logging(err);
-  });
+  })
 ;
